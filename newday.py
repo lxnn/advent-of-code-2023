bf30f40f2898afc1744e9ee9ffdcbe2dad8cab5c
Alex <lxnn.git@gmail.com>
2023-12-24 00:22:19 +0000

    Add input downloading to newday script

--- newday.py
+++ newday.py
@@ -1,7 +1,12 @@
-import sys, shutil, pathlib
+import os, sys, shutil, pathlib, requests, dotenv
+
+dotenv.load_dotenv()
 
 ROOT = pathlib.Path(__file__).parent
 TEMPLATE = ROOT / 'template.py'
+TOKEN = os.getenv('TOKEN')
+
+assert TOKEN is not None
 
 def main():
     day = int(sys.argv[1])
@@ -12,7 +17,16 @@ def main():
     folder.mkdir()
     shutil.copy(TEMPLATE, folder / 'solution.py')
     (folder / 'example').touch()
+    (folder / 'input').write_text(get_input(2023, day))
     return 0
+
+def get_input(year: int, day: int) -> str:
+    response = requests.get(
+        f'https://adventofcode.com/{year}/day/{day}/input',
+        cookies=dict(session=TOKEN),
+    )
+    response.raise_for_status()
+    return response.text
 
 if __name__ == '__main__':
     sys.exit(main())
