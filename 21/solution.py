eb0ecde962c2362fcd7926895e4217595cc47c07
Alex <lxnn.git@gmail.com>
2024-01-01 07:45:52 +0000

    Complete day 21 part 2

--- 21/solution.py
+++ 21/solution.py
@@ -1,5 +1,5 @@
 import sys
-import re
+import numpy as np
 
 
 def main():
@@ -18,19 +18,57 @@ type Coord = Vector
 def part1(raw: str):
     grid = parse(raw)
     (start,) = extract(grid, "S")
-    return len(reachable(grid, start, steps=64))
+    visualise(grid, possible_locations(grid, start, steps=6))
+    return len(possible_locations(grid, start, steps=6))
+
+
+P2_STEPS = 26501365
 
 
 def part2(raw: str):
-    ...
+    grid = parse(raw)
+    height, width = dimensions(grid)
+    x0 = P2_STEPS % height
+    x1 = x0 + height
+    x2 = x1 + height
+    tiled = tile(grid, 7, 7)
+    (start,) = extract(tiled, "S")
+    y0 = len(possible_locations(tiled, start, steps=x0))
+    y1 = len(possible_locations(tiled, start, steps=x1))
+    y2 = len(possible_locations(tiled, start, steps=x2))
+    A = np.array(
+        [
+            [x0**2, x0, 1],
+            [x1**2, x1, 1],
+            [x2**2, x2, 1],
+        ]
+    )
+    y = np.array([y0, y1, y2])
+    a, b, c = np.linalg.inv(A) @ y
+    return a * P2_STEPS**2 + b * P2_STEPS + c
 
 
-def reachable(grid: dict[Coord, str], start: Coord, steps: int) -> set[Coord]:
-    navigable = extract(grid, ".") | extract(grid, 'S')
-    positions = {start}
-    for step in range(64):
-        positions = neighbors(positions) & navigable
-    return positions
+def tile(grid, m, n):
+    height, width = dimensions(grid)
+    assert m % 2 == 1
+    assert n % 2 == 1
+    tiled_grid = {
+        coord + height * i + width * j * 1j: "." if char == "S" else char
+        for i in range(m)
+        for j in range(n)
+        for coord, char in grid.items()
+    }
+    for start in extract(grid, "S"):
+        tiled_grid[start + m // 2 * height + n // 2 * width * 1j] = "S"
+    return tiled_grid
+
+
+def possible_locations(grid: dict[Coord, str], start: Coord, steps: int) -> set[Coord]:
+    navigable = extract(grid, ".") | extract(grid, "S")
+    locations = {start}
+    for step in range(steps):
+        locations = neighbors(locations) & navigable
+    return locations
 
 
 def distance(a: Coord, b: Coord, grid: dict[Coord, str]) -> int:
@@ -47,14 +85,6 @@ def dimensions(grid: dict[Coord, str]) -> tuple[int, int]:
     height = int(1 + max(coord.real for coord in grid))
     width = int(1 + max(coord.imag for coord in grid))
     return height, width
-
-
-def corners(grid: dict[Coord, str]) -> list[Coord]:
-    NW_corner = 0 + 0j
-    SE_corner = max(grid, key=lambda coord: (coord.real, coord.imag))
-    NE_corner = 0 + SE_corner.imag * 1j
-    SW_corner = SE_corner.real + 0j
-    return [NW_corner, SE_corner, NE_corner, SW_corner]
 
 
 def parse(raw: str) -> dict[Coord, str]:
@@ -74,6 +104,36 @@ def neighbors(coords: Coord | set[Coord]) -> set[Coord]:
 
 def extract(grid: dict[Coord, str], target: str) -> set[Coord]:
     return {coord for coord, tile in grid.items() if tile == target}
+
+
+def visualise(grid: dict[Coord, str], filled: set[Coord], fillchar="O"):
+    height, width = dimensions(grid)
+    for r in range(height):
+        for c in range(width):
+            coord = r + c * 1j
+            if coord in filled:
+                print(fillchar, end="")
+            else:
+                print(grid[coord], end="")
+        print()
+
+
+def save_image(grid, filled, name):
+    with open(f"{name}.ppm", "w") as file:
+        file.write(to_ppm(grid, filled))
+
+
+def to_ppm(grid, filled):
+    height, width = dimensions(grid)
+    fill_colour = "0 0 255"
+    colour = {".": "255 255 255", "#": "0 0 0", "S": "0 255 0"}
+    return f"P3 {width} {height} 255\n" + "\n".join(
+        " ".join(
+            fill_colour if r + c * 1j in filled else colour[grid[r + c * 1j]]
+            for c in range(width)
+        )
+        for r in range(height)
+    )
 
 
 if __name__ == "__main__":
